c60fde3ecc9f1f8ee88ce614b8e9692944cdde22
Bryan Choe <bryan.choe88@gmail.com>
2017-04-03 16:49:26 -1000

    adds april prep students

--- js/app.js
+++ js/app.js
@@ -5,24 +5,26 @@ let febPrep = ['Christine', 'Jacob', 'Anthony', 'Earnest', 'Ann', 'Ella', 'James
 
 let marPrep = ['Brandon','J5','Kelsie','Mellanie','Seol Hee','Jason','Jeffe','Keola','Vasana'];
 
+let aprPrep = ['Brendan', 'Jody', 'Levi', 'Oksana', 'Ross', 'Scott', 'Virginia'];
+
 // es5 javascript
-/*function randomStudentGenerator(arr){
+function randomStudentGenerator(arr){
   var randomIndex = Math.floor(Math.random() * arr.length);
   var randomElement = arr[randomIndex];
   return randomElement;
 }
 
-console.log(randomStudentGenerator(febPrep));*/
+console.log(randomStudentGenerator(aprPrep));
 
 // es6 javascript
-let randomStudentGenerator = (arr) => {
-  let randomIndex = Math.floor(Math.random() * arr.length);
-  let randomElement = arr[randomIndex];
-  document.getElementById('main');
-  randomElement.innerHTML = randomElement;
-  return randomElement;
-};
+// let randomStudentGenerator = (arr) => {
+//   let randomIndex = Math.floor(Math.random() * arr.length);
+//   let randomElement = arr[randomIndex];
+//   document.getElementById('main');
+//   randomElement.innerHTML = randomElement;
+//   return randomElement;
+// };
 
-console.log(randomStudentGenerator(marPrep));
+// console.log(randomStudentGenerator(marPrep));
 
-document.getElementById("demo").addEventListener("click", randomStudentGenerator(marPrep));
+// document.getElementById("demo").addEventListener("click", randomStudentGenerator(marPrep));
